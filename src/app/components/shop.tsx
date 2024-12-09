9daced654aaad9f7ad930681bdd1b7bc8c13fa12
Muhammad Yousuf <160126398+muhammad-yousuf1@users.noreply.github.com>
2024-12-10 00:12:06 +0500

    Add files via upload

---
+++ src/app/components/shop.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { FaFilter } from 'react-icons/fa6';
+import { HiRectangleStack } from 'react-icons/hi2';
+import { IoTimeOutline } from 'react-icons/io5';
+import { PiCirclesFourFill } from 'react-icons/pi';
+
+function ShopLine() {
+  return (
+    <div className="bg-[#F4F4F4] py-4 px-6 flex justify-between items-center flex-wrap">
+      {/* Left Section */}
+      <div className="flex items-center gap-4 flex-wrap mb-4 sm:mb-0">
+        <div className="flex items-center gap-1 cursor-pointer">
+          <FaFilter className="text-xl" />
+          <p className='ml-2'>Filter</p>
+        </div>
+        <div className="flex items-center gap-2">
+          <PiCirclesFourFill className="text-xl ml-2 cursor-pointer" />
+          <HiRectangleStack className="text-xl ml-2 cursor-pointer" />
+        </div>
+        <div className="w-[2px] h-10 bg-gray-400 mx-4"></div>
+        <p>Showing 1-6 of 32 results</p>
+      </div>
+
+      {/* Right Section */}
+      <div className="flex items-center gap-4">
+        <div className="flex items-center gap-2">
+          <p>Show</p>
+          <div className="px-2 py-1 bg-white border border-gray-300 rounded-md cursor-pointer">16</div>
+        </div>
+        <div className="flex items-center gap-2">
+          <p>Sort by</p>
+          <div className="px-2 py-1 bg-white border border-gray-300 rounded-md cursor-pointer">Default</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ShopLine
